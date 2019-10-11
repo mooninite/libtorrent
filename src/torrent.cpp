769f875f4dde72db43b5125ac29d106054b91ab6
arvidn <arvid@libtorrent.org>
2019-10-11 11:19:33 +0200

    move towards socket_type not being default-constructible (i.e. towards it always holding a valid socket). Remove unnecessary extra io_context state in socket_type

--- src/torrent.cpp
+++ src/torrent.cpp
@@ -6084,7 +6084,7 @@ bool is_downloading_state(int const st)
 			&& web->have_files.none_set()) return;
 
 		std::shared_ptr<aux::socket_type> s
-			= std::make_shared<aux::socket_type>(m_ses.get_context());
+			= std::make_shared<aux::socket_type>();
 		if (!s) return;
 
 		void* userdata = nullptr;
@@ -6794,7 +6794,7 @@ bool is_downloading_state(int const st)
 			|| !m_ip_filter
 			|| (m_ip_filter->access(peerinfo->address()) & ip_filter::blocked) == 0);
 
-		std::shared_ptr<aux::socket_type> s = std::make_shared<aux::socket_type>(m_ses.get_context());
+		std::shared_ptr<aux::socket_type> s = std::make_shared<aux::socket_type>();
 
 #if TORRENT_USE_I2P
 		bool const i2p = peerinfo->is_i2p_addr;
